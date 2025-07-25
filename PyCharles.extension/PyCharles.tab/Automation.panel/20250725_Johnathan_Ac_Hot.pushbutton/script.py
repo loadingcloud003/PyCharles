006e46d08d1c2feae2e718e7fb7fe03e45bffccc
charleslo1997 <charleslo1997@gmail.com>
2025-07-25 17:42:25 +0800

    2025-07-25

--- PyCharles.extension/PyCharles.tab/Automation.panel/20250725_Johnathan_Ac_Hot.pushbutton/script.py
+++ PyCharles.extension/PyCharles.tab/Automation.panel/20250725_Johnathan_Ac_Hot.pushbutton/script.py
@@ -1,3 +1,85 @@
+def export_parts_and_references_to_excel_xml(doc, view, out_xml_path):
+    parts = get_all_parts_in_current_view(doc, view)
+    rows = []
+    all_part_param_names = set()
+    all_ref_param_names = set()
+    part_data = []
+    for part in parts:
+        part_id = part.Id.IntegerValue
+        part_fam, part_type, part_cat, part_params = get_element_info(part)
+        ref_elem = get_reference_element(doc, part)
+        ref_elem_id = ref_elem.Id.IntegerValue if ref_elem else ''
+        ref_fam, ref_type, ref_cat, ref_params = get_element_info(ref_elem)
+        all_part_param_names.update(part_params.keys())
+        all_ref_param_names.update(ref_params.keys())
+        part_data.append({
+            'part_element_id': part_id,
+            'part_family_name': part_fam,
+            'part_type_name': part_type,
+            'reference_element_id': ref_elem_id,
+            'reference_family_name': ref_fam,
+            'reference_type_name': ref_type,
+            'reference_category': ref_cat,
+            'part_params': part_params,
+            'reference_params': ref_params
+        })
+
+    # Show parameter selection dialog
+    part_param_list = sorted(all_part_param_names)
+    ref_param_list = sorted(all_ref_param_names)
+    selected_part_params = forms.SelectFromList.show(part_param_list, multiselect=True, title='Select Part Parameters to Export', button_name='Export')
+    if not selected_part_params:
+        script.exit('No Part parameters selected.')
+    selected_ref_params = forms.SelectFromList.show(ref_param_list, multiselect=True, title='Select Reference Element Parameters to Export', button_name='Export')
+    if not selected_ref_params:
+        script.exit('No Reference Element parameters selected.')
+    part_param_cols = ['part_param_' + k for k in selected_part_params]
+    ref_param_cols = ['reference_param_' + k for k in selected_ref_params]
+    fieldnames = [
+        'part_element_id', 'part_family_name', 'part_type_name',
+        'reference_element_id', 'reference_family_name', 'reference_type_name', 'reference_category'
+    ] + part_param_cols + ref_param_cols
+
+    # Build rows
+    for data in part_data:
+        row = [
+            data['part_element_id'],
+            data['part_family_name'],
+            data['part_type_name'],
+            data['reference_element_id'],
+            data['reference_family_name'],
+            data['reference_type_name'],
+            data['reference_category']
+        ]
+        for k in selected_part_params:
+            row.append(data['part_params'].get(k, ''))
+        for k in selected_ref_params:
+            row.append(data['reference_params'].get(k, ''))
+        rows.append(row)
+
+    # Write XML Spreadsheet 2003
+    with open(out_xml_path, 'w') as f:
+        f.write('<?xml version="1.0"?>\n')
+        f.write('<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">\n')
+        f.write('<Worksheet ss:Name="Parts and References">\n')
+        f.write('<Table>\n')
+        # Write header
+        f.write('<Row>')
+        for col in fieldnames:
+            f.write('<Cell><Data ss:Type="String">{}</Data></Cell>'.format(col))
+        f.write('</Row>\n')
+        # Write data rows
+        for row in rows:
+            f.write('<Row>')
+            for val in row:
+                # Escape XML special chars
+                sval = str(val).replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+                f.write('<Cell><Data ss:Type="String">{}</Data></Cell>'.format(sval))
+            f.write('</Row>\n')
+        f.write('</Table>\n')
+        f.write('</Worksheet>\n')
+        f.write('</Workbook>\n')
+    print('Exported {} part(s) info to: {}'.format(len(rows), out_xml_path))
 from pyrevit import forms
 # -*- coding: utf-8 -*-
 from pyrevit import revit, script
@@ -126,14 +208,15 @@ def export_parts_and_references_to_csv(doc, view, out_csv_path):
     print('Exported {} part(s) info to: {}'.format(len(rows), out_csv_path))
 
 # --- Main pyRevit command ---
+
 def main():
     doc = revit.doc
     view = doc.ActiveView
     # Use system temp directory for output
     import tempfile
     folder = tempfile.gettempdir()
-    out_csv = os.path.join(folder, 'parts_and_references.csv')
-    export_parts_and_references_to_csv(doc, view, out_csv)
+    out_xml = os.path.join(folder, 'parts_and_references.xml')
+    export_parts_and_references_to_excel_xml(doc, view, out_xml)
 
 if __name__ == '__main__':
     main()
